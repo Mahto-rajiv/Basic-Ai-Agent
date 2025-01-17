2f6636ad7753fb0c92d10ad3abf3cc26b92ade84
Rajiv Mahto <rajivmahto6355@gmail.com>
2025-01-17 21:06:50 +0530

    added socket.io integration and update package dependencies

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -87,8 +87,9 @@ export const getAllUsersController = async (req, res) => {
     const allUsers = await userService.getAllUsers({
       userId: loggedInUser._id,
     });
-    res.status(200).json({ allUsers });
+    res.status(200).json({ users: allUsers });
   } catch (error) {
     console.log(error);
+    return res.status(400).json({ error: error.message });
   }
 };
