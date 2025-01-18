ea920e70cc9cbf4f5b4ff2aae28ed51eafd3405e
Rajiv Mahto <rajivmahto6355@gmail.com>
2025-01-18 19:58:18 +0530

    Implement a project-based socket connection and user authentication; add a UserAuth component for route protection.

--- fronted/src/config/socket.js
+++ fronted/src/config/socket.js
@@ -2,12 +2,16 @@ import socket from "socket.io-client";
 
 let socketInstance = null;
 
-export const initializeSocket = () => {
+export const initializeSocket = (projectId) => {
   socketInstance = socket(import.meta.env.VITE_API_URL, {
     auth: {
       token: localStorage.getItem("token"),
     },
+    query: {
+      projectId,
+    },
   });
+
   return socketInstance;
 };
 
@@ -15,6 +19,6 @@ export const receiveMessage = (eventName, cb) => {
   socketInstance.on(eventName, cb);
 };
 
-export const sendMessage = (eventName, cb) => {
-  socketInstance.on(eventName, cb);
+export const sendMessage = (eventName, data) => {
+  socketInstance.emit(eventName, data);
 };
